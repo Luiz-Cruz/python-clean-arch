a4474ff65ff8129536ee2be095b821efadb579f6
Luiz Cruz <luiz.cruz@mercadolivre.com>
2023-07-05 19:18:59 -0300

    feat: implementing infra with config, entities, repo

---
+++ src/infra/repo/fake.py
@@ -0,0 +1,20 @@
+from src.infra.config import DBConnectionHandler
+from src.infra.entities import Users
+
+
+class FakerRepo:
+    """A simple Repository"""
+
+    @classmethod
+    def insert_user(cls):
+        """Some method to insert user"""
+        with DBConnectionHandler() as db_connection:
+            try:
+                new_user = Users(name="Luizera", password="Fallen")
+                db_connection.session.add(new_user)
+                db_connection.session.commit()
+            except:
+                db_connection.session.rollback()
+                raise
+            finally:
+                db_connection.session.close()
