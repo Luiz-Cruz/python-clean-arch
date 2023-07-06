abeb06a8ea74c56215dde459ee98a4d26729e7e0
Luiz Cruz <luiz.cruz@mercadolivre.com>
2023-07-05 22:23:31 -0300

    feat: Implementing models in domain layer

--- src/infra/repo/user_repository.py
+++ src/infra/repo/user_repository.py
@@ -1,6 +1,7 @@
+from src.domain.models import Users
 from src.infra.config import DBConnectionHandler
-from src.infra.entities import Users
-from collections import namedtuple
+from src.infra.entities import Users as UsersModel
+
 
 class UserRepository:
     """Class to manage User Repository"""
@@ -12,15 +13,15 @@ class UserRepository:
         :param password: User password
         :return: tuple with new user inserted
         """       
-        InsertData = namedtuple("Users", "id, name, password")
         with DBConnectionHandler() as db_connection:
             try:
-                new_user = Users(name=name, password= password)
+                new_user = UsersModel(name=name, password= password)
                 db_connection.session.add(new_user)
                 db_connection.session.commit()
-                return InsertData(id=new_user.id, name=new_user.name, password=new_user.password)
+                return Users(id=new_user.id, name=new_user.name, password=new_user.password)
             except Exception:
                 db_connection.session.rollback()
                 raise
             finally:
                     db_connection.session.close()
+                    
